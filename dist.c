3cb286c865e00a91824f31fbe25ead9042e84f25
bhagyashreemmural <muralbhagyashree7@gmail.com>
2025-11-21 11:00:58 +0530

    dist.c

---
+++ dist.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int x1,y1,x2,y2;
+    printf("enter the value for x1,y1,x2,y2");
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    float dist = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    printf("distance :%f",dist);
+}
+
+  
