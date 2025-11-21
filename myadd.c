3b3925c38f4d5f02240874a18eb49d665668aea2
bhagyashreemmural <muralbhagyashree7@gmail.com>
2025-11-21 11:06:54 +0530

    myadd.c

---
+++ myadd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int myadd(int a, int b)
+{
+    int sum = a + b ;
+    return sum;
+}
+void main()
+{
+    int a = 15;
+    int b = 85;
+    printf("%d\n",myadd(a,b));
+    int c = 88;
+    int d= 32;
+    printf("%d\n",myadd(c,d));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+}                                                                                                                          
