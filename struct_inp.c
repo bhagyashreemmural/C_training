3dc217fd4e989b29ba98cc02dd2b0ea697df9d4a
bhagyashreemmural <muralbhagyashree7@gmail.com>
2025-11-21 12:35:13 +0530

    struct_inp.c

---
+++ struct_inp.c
@@ -0,0 +1,28 @@
+//structure for name of book , author name,year//
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct book
+{
+char book[100];
+char author[100];
+int year;
+};
+void main()
+{
+  int n;
+  printf("enter n :");
+  scanf("%d",&n);
+  struct book b[3];
+  for(int i = 0; i < n; i++)
+  {
+ printf("enter book :");
+ scanf("%s",b[i].book);
+ printf("enter the author :");
+ scanf("%s",b[i].author);
+ printf("enter the year of publications :");
+ scanf("%d",&b[i].year);
+
+  }
+
+}
