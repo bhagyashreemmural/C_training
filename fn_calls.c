7e5b392c1d0bbc61aac14e547418ba51adc5f4f3
bhagyashreemmural <muralbhagyashree7@gmail.com>
2025-11-21 15:47:49 +0530

    fn_calls.c

---
+++ fn_calls.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void sum_value(int a,int b)
+{
+    int sum = a+b;
+    printf("%d",sum);
+
+}
+void sum_ref(int *a,int *b)
+{
+    int sum = *a + *b;
+    printf("%d",sum);
+
+}
+void main()
+{
+    int a = 48;
+    int b = 87;
+    printf("call by value \n");
+    sum_value(a,b);
+    printf("call by reference\n ");
+    sum_ref(&a,&b);
+    
+}
