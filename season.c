3e1934f09b363ab066a9c105a8c0ffe47e1fcb5b
bhagyashreemmural <muralbhagyashree7@gmail.com>
2025-11-22 14:55:26 +0530

    season.c

---
+++ season.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+enum month
+{
+    jan = 1,
+    feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec
+};
+void main()
+{
+    enum month m;
+    m = apr;
+    char *season;
+    switch(m)
+    {
+        case dec:case jan:case feb:
+        season = "winter";break;
+        case mar:case apr:case may:
+        season = "summer";break;
+        case jun:case jul:case aug:
+        season = "monsoon";break;
+        case sep:case oct:case nov:
+        season = "spring";break;
+    }
+    
+    printf("season is :%s",season);
+}
